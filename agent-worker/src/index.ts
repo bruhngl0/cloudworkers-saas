5458572b43972573d8396b3039da29de052402e1
bruhngl0 <adityashrm500@gmail.com>
2025-02-10 01:01:54 +0530

    db-integrated/agent-woker

--- agent-worker/src/index.ts
+++ agent-worker/src/index.ts
@@ -1,9 +1,44 @@
 import { Hono } from 'hono'
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { cors } from 'hono/cors';
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.text('build an ai-agent that takes the data from the queue and performs an ai logic on it')
-})
+app.use(cors())
+
+const prismaConnect = (dbUrl : string)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl: dbUrl
+  }).$extends(withAccelerate())
+
+  return prisma
+}
+
+app.get('/', async (c) => {
+  const prisma = prismaConnect(c.env.DATABASE_URL);
+  
+  try {
+  
+    const response = await prisma.quote.findMany({});
+ 
+
+    return c.json({
+      message: "Data fetched successfully",
+      data: response,
+    });
+  } catch (error) {
+    
+    return c.json({
+      message: "Error fetching data",
+    }, 500);
+  } finally {
+    await prisma.$disconnect();
+  }
+});
 
 export default app
